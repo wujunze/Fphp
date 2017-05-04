d58c6e04da578a08b3c96b675bc91076f8696734
wujunze <itwujunze@163.com>
2017-05-04 21:37:20 +0800

    框架配置文件

---
+++ conf/conf.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * @author     : wujunze
+ * @link       : https://wujunze.com
+ * @since      : 2017/5/4 下午8:41
+ * @filesource : conf.php
+ * @brief      : 配置文件
+ */
+
+return $conf = [
+  'db' => [
+      'host'    => '',
+      'port'    => '',
+      'user'    => '',
+      'pass'    => '',
+      'charset' => 'utf8',
+  ],
+  'default' => [
+      'Controller' => 'Item',
+      'Action'     => 'index',
+      ''
+  ],
+];
